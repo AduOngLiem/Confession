9e9da387047f99a325fa26fa4840702cf3586786
AduOngLiem <dinhcongthanhliem@gmail.com>
2025-12-17 21:56:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 const text = "Nè em...";
-let index =0;
+let index = 0;
 
-function type(){
-if(index < text.length){
-document.getElementById("typing").innerHTML += text.charAt(index);
-index++;
-setTimeout(type, 120);
-}
+function type() {
+  if (index < text.length) {
+    document.getElementById("typing").innerHTML += text.charAt(index);
+    index++;
+    setTimeout(type, 120);
+  }
 }
 type();
 
-function official(){
-const response = document.getElementById("response");
-const video = document.getElementById("surpriseVideo");
+function official() {
+  const response = document.getElementById("response");
+  const video = document.getElementById("surpriseVideo");
+  const final = document.getElementById("finalMessage");
 
-response.innerText = "Tụi mình chính thức rồi nha 💗";
-video.classList.add("show");
-video.play();
+  response.innerText = "Vậy là tụi mình chính thức rồi ha 💗";
+
+  setTimeout(() => {
+    video.classList.add("show");
+    video.play();
+  }, 900);
+
+  video.onended = () => {
+    final.style.display = "block";
+  };
 }
